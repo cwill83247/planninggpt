9faf3a52f3a0adac00d3f0573840a0a65b738143
cwill83247 <breakthrumusic@hotmail.com>
2023-04-26 10:16:17 +0000

    Initial Commit

---
+++ cw.py
@@ -0,0 +1,35 @@
+### https://colab.research.google.com/drive/1PQXcM_jhN6QJ7uTkxvNbxoI54r03uSr3?usp=sharing#scrollTo=UelAqQgk_yIt
+
+from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
+from langchain import OpenAI
+import sys
+import os
+from IPython.display import Markdown, display
+
+
+
+##Indexing Data anf outputting 2index.json"
+def construct_index(directory_path):
+    # set maximum input size
+    max_input_size = 4096
+    # set number of output tokens
+    num_outputs = 2000
+    # set maximum chunk overlap
+    max_chunk_overlap = 20
+    # set chunk size limit
+    chunk_size_limit = 600 
+
+    # define prompt helper
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+    # define LLM
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-davinci-003", max_tokens=num_outputs))
+ 
+    documents = SimpleDirectoryReader(directory_path).load_data()
+    
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+
+    index.save_to_disk('index.json')
+
+    return index
